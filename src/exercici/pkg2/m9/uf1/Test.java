e5d48b0adce4bfbc79eee091bfbfdf4577d2c121
davidblancolopez <daviddbl94@gmail.com>
2017-04-21 19:00:16 +0200

    Cambios

--- src/exercici/pkg2/m9/uf1/Test.java
+++ src/exercici/pkg2/m9/uf1/Test.java
@@ -2,13 +2,12 @@
 package exercici.pkg2.m9.uf1;
 
 
-public class Exercici2M9UF1 {
+public class Test {
 
 
     public static void main(String[] args) {
         Usuari us = new Usuari("nombre", "apellido", "apellido2" ,"12A");
-        
-        System.out.println(us.getUsuari());
+
         
     }
     
--- src/exercici/pkg2/m9/uf1/fitxerUsuaris.java
+++ src/exercici/pkg2/m9/uf1/fitxerUsuaris.java
@@ -2,16 +2,19 @@
 package exercici.pkg2.m9.uf1;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import javax.crypto.SecretKey;
 
 
 public class fitxerUsuaris {
     
-    File ficherUsuaris = new File("usuarios.txt");
+    File fitxerUsuaris = new File("usuarios.txt");
     String contrasenya = "DAM_M09_FERRERIA$13-14";
     SecretKey clau;
     String cadena_iv = "0123456789ABCDEF"; 
-
+    byte [] cadena = new byte[1024];
     
     private void addKey(){
         
@@ -25,5 +28,12 @@ public class fitxerUsuaris {
         
     }
     
+    private void adduserFile() throws FileNotFoundException{
+        FileInputStream fis = new FileInputStream(fitxerUsuaris);
+    }
+    
+    private void showFile() throws FileNotFoundException{
+        FileOutputStream fos = new FileOutputStream(fitxerUsuaris);
+    }
     
 }
