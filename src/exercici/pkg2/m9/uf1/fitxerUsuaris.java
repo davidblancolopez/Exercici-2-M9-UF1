82d4e1ede69881821f714cd47bd5ac454cb8db56
davidblancolopez <daviddbl94@gmail.com>
2017-04-21 17:33:54 +0200

    Cambios

--- src/exercici/pkg2/m9/uf1/fitxerUsuaris.java
+++ src/exercici/pkg2/m9/uf1/fitxerUsuaris.java
@@ -1,7 +1,24 @@
 
 package exercici.pkg2.m9.uf1;
 
+import java.io.File;
+
 
 public class fitxerUsuaris {
+    
+    File ficherUsuaris = new File("usuarios.txt");
+    
+    private void addKey(){
+        
+    }
+    
+    private void encryptFile(){
+        
+    }
+    
+    private void decryptFile(){
+        
+    }
+    
     
 }
