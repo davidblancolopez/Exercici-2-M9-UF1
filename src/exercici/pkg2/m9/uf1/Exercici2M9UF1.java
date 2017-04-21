30953880a5bcb3234b3c18b26db349938bf2ac16
davidblancolopez <daviddbl94@gmail.com>
2017-04-21 17:43:51 +0200

    Cambios

--- src/exercici/pkg2/m9/uf1/Exercici2M9UF1.java
+++ src/exercici/pkg2/m9/uf1/Exercici2M9UF1.java
@@ -6,6 +6,9 @@ public class Exercici2M9UF1 {
 
 
     public static void main(String[] args) {
+        Usuari us = new Usuari("nombre", "apellido","12A");
+        
+        System.out.println(us.getUsuari());
         
     }
     
--- src/exercici/pkg2/m9/uf1/Usuari.java
+++ src/exercici/pkg2/m9/uf1/Usuari.java
@@ -1,6 +1,13 @@
 
 package exercici.pkg2.m9.uf1;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
 
 public class Usuari {
     private String dni;
@@ -9,14 +16,46 @@ public class Usuari {
     private String usuari;
     private String password;
 
-    public Usuari(String dni, String nom, String cognom) {
+    public Usuari(String nom, String cognom, String dni) {
         this.dni = dni;
         this.nom = nom;
         this.cognom = cognom;
+        
+        this.usuari = nom + cognom + dni;
+    }
+    
+    public String getUsuari(){
+        return usuari;
     }
     
     private void xifrarPassword(){
         
+    }
+    
+    
+    
+    /**
+     * Metodo para generar la clave a partir del nombre de usuario.
+     *
+     * @param usuari
+     * @param tamany
+     * @return
+     */
+    public SecretKey generarClau(String usuari, int tamany) throws UnsupportedEncodingException {
+        SecretKey clau = null;
+
+        if ((tamany == 128) || (tamany == 192) || (tamany == 256)) {
+            try {
+                byte[] data = usuari.getBytes("UTF-8");
+                MessageDigest md = MessageDigest.getInstance("SHA-256");
+                byte[] hash = md.digest(data);
+                byte[] key = Arrays.copyOf(hash, tamany / 8);
+                clau = new SecretKeySpec(key, "AES");
+            } catch (NoSuchAlgorithmException ex) {
+                System.err.println("Generador no disponible.");
+            }
+        }
+        return clau;
     }
     
     
